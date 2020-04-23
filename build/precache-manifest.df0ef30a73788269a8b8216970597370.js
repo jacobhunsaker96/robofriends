34a69b0d0b02359aed498976d2e638c3525dc276
jacobhunsaker96 <jacob.hunsaker96@gmail.com>
2020-04-23 17:26:32 +0900

    adding files

---
+++ build/precache-manifest.df0ef30a73788269a8b8216970597370.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db8d09ab90ca7bc7674954f712566e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "573e9c90aacac20c725d",
+    "url": "/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "54314caf0a226f13fd83",
+    "url": "/static/css/main.6c1c3ccc.chunk.css"
+  },
+  {
+    "revision": "573e9c90aacac20c725d",
+    "url": "/static/js/2.8fde3f13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8fde3f13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54314caf0a226f13fd83",
+    "url": "/static/js/main.f8483000.chunk.js"
+  },
+  {
+    "revision": "65c6441c6da975648346",
+    "url": "/static/js/runtime-main.a210e4d1.js"
+  },
+  {
+    "revision": "5ce3fc11b995237a76e13dba89a9b3ea",
+    "url": "/static/media/Robot World.5ce3fc11.woff"
+  }
+]);
